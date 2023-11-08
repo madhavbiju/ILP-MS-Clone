b06bf08f1ce261521a0e0b2fb733ebfb872e0da1
Madhav B <madhav.b@experionglobal.local>
2023-11-08 13:06:13 +0530

    feat: Add a slideshow

---
+++ js/xbox_slider.js
@@ -0,0 +1,24 @@
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 5000); // Change image every 5 seconds
+
+  // Add code to hide all captions
+  var captions = document.getElementsByClassName("overlay");
+  for (i = 0; i < captions.length; i++) {
+    captions[i].style.display = "none";
+  }
+  // Display the caption for the current slide
+  captions[slideIndex - 1].style.display = "block";
+}
