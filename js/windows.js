352175d3561d5146911ffa4e4470782ea295e196
Navya George <navya.george@experionglobal.local>
2023-11-09 11:02:27 +0530

    feat:- added windows page

---
+++ js/windows.js
@@ -0,0 +1,12 @@
+function myButton() {
+    location.replace("https://www.microsoft.com/en-in/windows/get-windows-11")
+  }
+  function myButtonCopilot() {
+    location.replace("https://www.microsoft.com/en-in/windows/copilot-ai-features")
+  }
+  function exploreButton(){
+    location.replace("https://www.microsoft.com/en-in/windows/sync-across-your-devices")
+  }
+  function helpMeChoose(){
+    location.replace("https://www.microsoft.com/en-in/windows/sync-across-your-devices")
+  }
