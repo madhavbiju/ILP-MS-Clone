0ee06e4515184cb7c16c78d8c001a7f48407b8cf
Ashna Saju <ashna.saju@experionglobal.local>
2023-11-09 10:58:23 +0530

    feat:-The design is completed

---
+++ js/support.js
@@ -0,0 +1,18 @@
+// // Define the URL of the Microsoft API
+// const apiUrl = 'https://dummy.json/data';
+
+// // Make a GET request to the Microsoft API
+// fetch(apiUrl)
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error('Network response was not ok');
+//     }
+//     return response.json(); // Assuming the API returns JSON data
+//   })
+//   .then(data => {
+//     // Handle the data
+//     console.log(data);
+//   })
+//   .catch(error => {
+//   console.error(error);
+//   });
