7e4cd96c58a547f972d422aa264352a17bcad43c
Aljo Biju <aljo.biju@experionglobal.local>
2023-11-09 15:04:10 +0530

    feat:-images fetched from api

--- js/main.js
+++ js/main.js
@@ -1,40 +1,3 @@
-// // Function to fetch images from an API
-// async function fetchImages() {
-//     try {
-//       const response = await fetch('https://dummyjson.com/users'); // Replace with the actual image API endpoint
-//       const data = await response.json();
-//       return data;
-//     } catch (error) {
-//       console.error('Error fetching images:', error);
-//     }
-//   }
-
-//   // Function to create dynamic divs with fetched images
-//   async function createDynamicDivs() {
-//     const container = document.getElementById('divContainer');
-//     const images = await fetchImages();
-
-//     if (images) {
-//       images.forEach(image => {
-//         const div = document.createElement('div');
-//         div.classList.add('dynamicDiv');
-//         div.style.backgroundImage = `url(${image.imageUrl})`; // Assuming the API returns an 'imageUrl' property
-
-//         // You can add additional styles or content to the created divs
-//         // For example:
-//         div.style.width = '200px';
-//         div.style.height = '200px';
-//         div.style.backgroundSize = 'cover';
-//         div.style.margin = '10px';
-//         // Additional styles as needed
-
-//         container.appendChild(div);
-//       });
-//     }
-//   }
-
-//   // Call the function to create dynamic divs with images
-//   createDynamicDivs();
 
 const apiURL = "https://dummyjson.com/products";
 fetch(apiURL)
@@ -48,6 +11,16 @@ fetch(apiURL)
   .then((data) => {
     // if (data.status === "success") {
     const products = data.products;
+
+    
+
+
+
+
+
+
+
+
     products.slice(0, 2).forEach((product) => {
       console.log(product.thumbnail);
       var container = document.querySelector(`.column2`);
