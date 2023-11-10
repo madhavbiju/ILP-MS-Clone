5067c66c981a48f99ba13469e6a9263215236543
Navya George <navya.george@experionglobal.local>
2023-11-11 01:19:50 +0530

    feat:- windos page updated

--- js/windows.js
+++ js/windows.js
@@ -1,52 +1,89 @@
 function myButton() {
-    location.replace("https://www.microsoft.com/en-in/windows/get-windows-11")
-  }
-  function myButtonCopilot() {
-    location.replace("https://www.microsoft.com/en-in/windows/copilot-ai-features")
-  }
-  function exploreButton(){
-    location.replace("https://www.microsoft.com/en-in/windows/sync-across-your-devices")
-  }
-  function helpMeChoose(){
-    location.replace("https://www.microsoft.com/en-in/windows/sync-across-your-devices")
-  }
+  location.replace("https://www.microsoft.com/en-in/windows/get-windows-11");
+}
+function myButtonCopilot() {
+  location.replace(
+    "https://www.microsoft.com/en-in/windows/copilot-ai-features"
+  );
+}
+function exploreButton() {
+  location.replace(
+    "https://www.microsoft.com/en-in/windows/sync-across-your-devices"
+  );
+}
+function helpMeChoose() {
+  location.replace(
+    "https://www.microsoft.com/en-in/windows/sync-across-your-devices"
+  );
+}
+const request = fetch(
+  "https://dummyjson.com/products?limit=8&select=title,description,thumbnail,brand"
+);
 
-// const url = "https://dummyjson.com/products";
-// fetch(url)
-//   .then((response) => {
-//     let i = 0;
-//     if (!response.ok) {
-//       throw new Error("Network response was not OK");
-//     }
-//     return response.json();
-//   })
-//   //   .then(data => {
-//   //     for (let i = 0; i <= 0; i++) {
-//   //     const titleContent = document.querySelector('.container--b1');
-//   //     const titleImage = document.createElement('img');
-//   //     titleImage.setAttribute('src',data.products[i].images[0]);
-//   //     titleImage.classList.add('img');
-//   //     titleContent.appendChild(titleImage);
-//   //     }
+request
+  .then((response) => response.json())
+  .then((data) => {
+    const productsArray = data.products;
+    console.log(data.products);
+    const contentContainer = document.querySelector(".image-container-b");
 
-//   // }
-//   //   )
-//   //   .catch(error => {
-//   //     console.error('Fetch error:', error.message);
-//   //   });
-//   .then((data) => {
-//     const copilotContainer = document.querySelector(".image-container-b1");
-//     // const copilotdetails = document.querySelector(".copilot-details");
-//     // Generate a random index to select a different image each time
-//     for(let i=0;i<3;i++){
+    productsArray.slice(0, 3).forEach((product) => {
+      const desc = product.description;
+      const imageUrl = product.thumbnail;
+      const imagecontainerb1 = document.createElement("div");
+      imagecontainerb1.classList.add("image-container-b1");
+      const descElement = document.createElement("p");
+      descElement.textContent = `${desc}`;
+      const aElement = document.createElement("a");
+      aElement.textContent = "See what's New >";
+      aElement.href = "#";
+
+      const image = document.createElement("img");
+      image.classList.add("img-b");
+      image.src = imageUrl;
+      imagecontainerb1.appendChild(image);
+      imagecontainerb1.appendChild(descElement);
+      imagecontainerb1.appendChild(aElement);
+      contentContainer.appendChild(imagecontainerb1);
+    });
+
+    //---------
+
+    const containeri = document.querySelector(".container--i");
+
+    productsArray.slice(5, 8).forEach((product) => {
+      const desc = product.description;
+      const imageUrl = product.thumbnail;
+      const imagecontaineri= document.createElement("div");
+      imagecontaineri.classList.add("image-container-i");
+      const descElement = document.createElement("p");
+      descElement.textContent = `${desc}`;
+      const aElement = document.createElement("a");
+      aElement.textContent = "Explore Windows app >";
+      aElement.href = "#";
+
+      const image = document.createElement("img");
+      image.classList.add("img-i");
+      image.src = imageUrl;
+      imagecontaineri.appendChild(image);
+      imagecontaineri.appendChild(descElement);
+      imagecontaineri.appendChild(aElement);
+      containeri.appendChild(imagecontaineri);
+
+
      
-//     const randomIndex = Math.floor(Math.random() * data.products.length);
+    
 
-//     const copilotImages = document.createElement("img");
-//     copilotImages.setAttribute("src", data.products[randomIndex].thumbnail); // Assuming images is an array
-//     copilotImages.classList.add("img");
-//     copilotContainer.appendChild(copilotImages);}
-//   })
-//   .catch((error) => {
-//     console.error("Fetch error:", error.message);
-//   });
+    });
+
+    //--------
+  })
+
+  
+  .catch((error) => {
+    console.error("Error fetching data: ", error);
+  });
+
+
+
+ 
