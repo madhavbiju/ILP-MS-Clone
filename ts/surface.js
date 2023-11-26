bbf1b4e70c8c63b6eb81780219e0673b618454e8
Aljo Biju <aljo.biju@experionglobal.local>
2023-11-27 03:46:16 +0530

    fix:-surface support typescript added

--- ts/surface.js
+++ ts/surface.js
@@ -1,38 +1,4 @@
 "use strict";
-// import { Data,Product } from "../types/surface";
-// const apiURL = "https://mocki.io/v1/c227a5f1-333a-4c41-ac9e-9384a4bbd37a";
-// fetch(apiURL)
-//   .then((response) => {
-//     if (response.ok) {
-//       return response.json();
-//     } else {
-//       throw new Error("Failed to fetch data");
-//     }
-//   })
-//   .then((data) => {
-//     // if (data.status === "success") {
-//     const products = data.products;
-//     products.slice(0, 2).forEach((product) => {
-//       console.log(product.thumbnail);
-//       var container = document.querySelector(`.column2`);
-//       //   var container = document.getElementsByClassName(column2);
-//       var row = document.createElement("div");
-//       row.classList.add("row1");
-//       //   const productImage = product.thumbnail;
-//       var image = document.createElement("img");
-//       image.src = product.thumbnail;
-//       var anchor1 = document.createElement("a");
-//       anchor1.classList.add("row-anchor");
-//       anchor1.href = "#";
-//       anchor1.textContent = product.title;
-//       row.appendChild(image);
-//       row.appendChild(anchor1);
-//       container.appendChild(row);
-//     });
-//   })
-//   .catch((error) => {
-//     console.error(error.message);
-//   });
 Object.defineProperty(exports, "__esModule", { value: true });
 var apiURL = "https://mocki.io/v1/c227a5f1-333a-4c41-ac9e-9384a4bbd37a";
 // Fetch and render first 2 products in the 'column2' container
