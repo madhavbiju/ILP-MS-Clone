116d047cfe23a5d862476adf5d3af5999c35328f
Aljo Biju <aljo.biju@experionglobal.local>
2023-11-27 09:19:11 +0530

    feat:- surface support ts done

--- ts/surface_support.js
+++ ts/surface_support.js
@@ -12,23 +12,21 @@ fetch(apiURL)
     .then(function (data) {
     var products = data.products.slice(0, 7); // Limit to 7 images
     var container = document.getElementById("dynamicDivsContainer");
-    if (container) {
-        var row_1 = document.createElement("div");
-        row_1.classList.add("row1");
-        products.forEach(function (product, index) {
-            var imageContainer = createImageContainer(product.thumbnail, product.title);
-            row_1.appendChild(imageContainer);
-            // If 4 images have been added or it's the last image, create a new row
-            if ((index + 1) % 4 === 0 || index === products.length - 1) {
-                container.appendChild(row_1);
-                // Create a new row for the next set of images
-                if (index !== products.length - 1) {
-                    row_1 = document.createElement("div");
-                    row_1.classList.add("row1");
-                }
+    var row = document.createElement("div");
+    row.classList.add("row1");
+    products.forEach(function (product, index) {
+        var imageContainer = createImageContainer(product.thumbnail, product.title);
+        row.appendChild(imageContainer);
+        // If 4 images have been added or it's the last image, create a new row
+        if ((index + 1) % 4 === 0 || index === products.length - 1) {
+            container.appendChild(row);
+            // Create a new row for the next set of images
+            if (index !== products.length - 1) {
+                row = document.createElement("div");
+                row.classList.add("row1");
             }
-        });
-    }
+        }
+    });
 })
     .catch(function (error) {
     console.error(error.message);
@@ -43,25 +41,23 @@ fetch(apiResponse)
     return response.json();
 })
     .then(function (data) {
+    var products = data.products.slice(0, 7); // Limit to 7 images
     var container = document.getElementById("dynamicDivsContainerMid");
-    if (container) {
-        var products_1 = data.products.slice(0, 7); // Limit to 7 images
-        var row_2 = document.createElement("div");
-        row_2.classList.add("row1");
-        products_1.forEach(function (product, index) {
-            var imageContainer = createImageContainer(product.thumbnail, product.title);
-            row_2.appendChild(imageContainer);
-            // If 4 images have been added or it's the last image, create a new row
-            if ((index + 1) % 4 === 0 || index === products_1.length - 1) {
-                container.appendChild(row_2);
-                // Create a new row for the next set of images
-                if (index !== products_1.length - 1) {
-                    row_2 = document.createElement("div");
-                    row_2.classList.add("row1");
-                }
+    var row = document.createElement("div");
+    row.classList.add("row1");
+    products.forEach(function (product, index) {
+        var imageContainer = createImageContainer(product.thumbnail, product.title);
+        row.appendChild(imageContainer);
+        // If 4 images have been added or it's the last image, create a new row
+        if ((index + 1) % 4 === 0 || index === products.length - 1) {
+            container.appendChild(row);
+            // Create a new row for the next set of images
+            if (index !== products.length - 1) {
+                row = document.createElement("div");
+                row.classList.add("row1");
             }
-        });
-    }
+        }
+    });
 })
     .catch(function (error) {
     console.error(error.message);
@@ -69,7 +65,7 @@ fetch(apiResponse)
 // Function to create an image container
 function createImageContainer(imageUrl, title) {
     var imageContainer = document.createElement("div");
-    imageContainer.classList.add("image-container col-");
+    imageContainer.classList.add("image-container");
     var image = document.createElement("img");
     image.src = imageUrl;
     var titleElement = document.createElement("a");
